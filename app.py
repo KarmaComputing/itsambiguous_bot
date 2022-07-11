ce60e1174505b07bf8a948d35f9690480ba97a08
chrisjsimpson <chris.j.simpson@live.co.uk>
2022-07-11 19:56:50 +0100

    extend timeout

--- app.py
+++ app.py
@@ -19,7 +19,8 @@ async def index(request):
     if "it" in body["message"]["text"].lower():
         reply = "Pardon me, what do you mean by 'it' exactly? Please avoid ambiguous words and phrases. Your loving bot friend. Saving you time, and removing the perils of ambiguity ❤️."  # noqa: E501
         requests.get(
-            f"https://api.telegram.org/bot{TELEGRAM_API_TOKEN}/sendMessage?chat_id={TELEGRAM_CHAT_ID}&text={reply}", timeout=0.001  # noqa: E501
+            f"https://api.telegram.org/bot{TELEGRAM_API_TOKEN}/sendMessage?chat_id={TELEGRAM_CHAT_ID}&text={reply}",
+            timeout=10,  # noqa: E501
         )
     return PlainTextResponse("OK")
 
